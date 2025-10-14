54968558998e19884140611e9033ab3fd6e028d0
Adix-null <adomas@stalas.lt>
2025-10-14 14:26:41 +0000

    1 cli command works withut persistance

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -2,188 +2,31 @@ module Lib1
   ( examples,
     Command (..),
     Dumpable (..),
+    File (..),
   )
 where
-
-import GHC.Generics (D)
 
 data Dumpable = Examples
   deriving (Show)
 
-type AlphanumStr = String
-
-type Name = String
-
-type DataStr = String
-
-type Extension = String
-
-type Path = String
-
-type ASCII = Char
-
+-- Represents a file (name + extension)
 data File = File
-  { name :: Name,
-    fileData :: DataStr
+  { name :: String,
+    ext :: String
   }
-  deriving (Show, Eq)
-
-data Folder = Folder
-  { folderName :: AlphanumStr,
-    contents :: [FolderContent]
-  }
-  deriving (Show, Eq)
-
-data FolderContent
-  = FileRec File
-  | FolderRec Folder
-  deriving (Show, Eq)
-
-data AzAZ09
-  = Lower Char
-  | Upper Char
-  | Digit Char
-  deriving (Show, Eq)
-
-data Symbol
-  = SymTab
-  | SymExcl
-  | SymQuote
-  | SymDollar
-  | SymPercent
-  | SymAmp
-  | SymApos
-  | SymLParen
-  | SymRParen
-  | SymStar
-  | SymPlus
-  | SymComma
-  | SymMinus
-  | SymDot
-  | SymSlash
-  | SymColon
-  | SymSemi
-  | SymLt
-  | SymEq
-  | SymGt
-  | SymQMark
-  | SymAt
-  | SymBackslash
-  | SymCaret
-  | SymUnderscore
-  | SymBacktick
-  | SymLCurly
-  | SymPipe
-  | SymRCurly
-  | SymTilde
-  deriving (Show, Eq)
-
-data Ascii
-  = Aza AzAZ09
-  | Sym Symbol
-  deriving (Show, Eq)
-
-data FileExt
-  = Txt
-  | Png
-  | Jpg
-  | Json
-  | Dat
-  | Exe
-  | Hs
-  | Cs
-  | Html
-  | Cpp
-  | Mp4
-  | Mp3
-  deriving (Show, Eq)
-
-data Command
-  = Dump Dumpable
-  | AddFile Path File
-  | DeleteFile Path Name
-  | AddFolder Path AlphanumStr
-  | DeleteFolder Path
-  | MoveFolder Path Path
-  | MoveFile Path Path Name
-  | PrintFS
   deriving (Show)
 
-apply :: Command -> Command -> Command
-apply (Dump _) fs = fs
+-- Available commands
+data Command
+  = Dump Dumpable
+  | PrintFile
+  | EditFile String
+  deriving (Show)
 
--- insertFile :: [String] -> String -> String -> String -> FS -> FS
--- insertFile _ _ _ _ fs = fs
-
--- deleteFile :: [String] -> String -> String -> FS -> FS
--- deleteFile _ _ _ fs = fs
-
-insertFolder :: Folder -> Path -> AlphanumStr -> Either String Folder
-insertFolder filesystem path newName =
-  case splitPath path of
-    [] -> Left "Empty path provided"
-    [target] ->
-      if folderName filesystem == target
-        then
-          Right
-            ( filesystem
-                { contents = contents filesystem ++ [FolderRec (Folder newName [])]
-                }
-            )
-        else Left ("Folder not found: " ++ target)
-    (p : ps) ->
-      if folderName filesystem == p
-        then
-          let updateSub :: FolderContent -> Either String FolderContent
-              updateSub (FolderRec f) =
-                ( case insertFolder f (joinPath ps) newName of
-                    Left err -> Left err
-                    Right f' -> Right (FolderRec f')
-                )
-              updateSub other = Right other
-
-              updatedContents = map updateSub (contents filesystem)
-           in ( case sequence updatedContents of
-                  Left err -> Left err
-                  Right cs ->
-                    Right
-                      ( filesystem
-                          { contents = cs
-                          }
-                      )
-              )
-        else Left ("Folder not found: " ++ p)
-
--- deleteFolder :: [String] -> FS -> FS
--- deleteFolder _ fs = fs
-
--- moveFile :: [String] -> [String] -> String -> String -> FS -> FS
--- moveFile _ _ _ _ fs = fs
-
--- moveFolder :: [String] -> [String] -> FS -> FS
--- moveFolder _ _ fs = fs
-
--- printFS :: FS -> FS
--- printFS fs = fs
-
--- helper functions
-splitPath :: Path -> [String]
-splitPath "" = []
-splitPath p = case break (== '/') p of
-  (seg, "") -> [seg]
-  (seg, _ : rest) -> seg : splitPath rest
-
-joinPath :: [String] -> Path
-joinPath = foldr1 (\a b -> a ++ "/" ++ b)
-
+-- Example commands
 examples :: [Command]
 examples =
   [ Dump Examples,
-    -- AddFile ["/"] "file1" "txt" "Data of file",
-    -- DeleteFile ["/"] "file1" "txt",
-    AddFolder "/" "folder1",
-    -- DeleteFolder ["/", "folder1"],
-    -- MoveFile ["/"] ["folder1"] "file1" "txt",
-    -- MoveFolder ["/", "folder1"] ["/"],
-    PrintFS
+    PrintFile,
+    EditFile "abcd"
   ]
--- src/Lib2.hs
+++ src/Lib2.hs
@@ -8,35 +8,51 @@ module Lib2
   )
 where
 
-import Data.List (stripPrefix)
 import qualified Lib1
 
 type ErrorMsg = String
 
 type Parser a = String -> Either ErrorMsg (a, String)
 
--- | Parses user's input.
+-- Simple parser for commands
 parseCommand :: Parser Lib1.Command
-parseCommand input
-  | Just rest <- stripPrefix "AddFolder " input,
-    (path, ' ' : name) <- break (== ' ') rest =
-      Right (Lib1.AddFolder path name, "") -- remaining string is empty
-  | otherwise = Left "Command not recognized"
+parseCommand input =
+  case words input of
+    ["Dump", "Examples"] -> Right (Lib1.Dump Lib1.Examples, "")
+    ["PrintFile"] -> Right (Lib1.PrintFile, "")
+    ["EditFile", name] -> Right (Lib1.EditFile name, "")
+    _ -> Left "Invalid command"
 
+-- Default file for demo purposes
+defaultFile :: Lib1.File
+defaultFile = Lib1.File "default" "txt"
+
+-- Process command (pure, no state stored between calls)
 process :: Lib1.Command -> [String]
-process (Lib1.Dump Lib1.Examples) = "Examples:" : map toCliCommand Lib1.examples
-process c = ["Parsed as " ++ toCliCommand c]
+process (Lib1.Dump Lib1.Examples) =
+  "Examples:" : map toCliCommand Lib1.examples
+process Lib1.PrintFile =
+  [Lib1.name defaultFile ++ "." ++ Lib1.ext defaultFile]
+process (Lib1.EditFile newName) =
+  let edited = defaultFile {Lib1.name = newName}
+   in ["File renamed (not persisted): " ++ Lib1.name edited ++ "." ++ Lib1.ext edited]
+process c =
+  ["Parsed as " ++ show c]
 
+-- Class for converting commands to strings
 class ToCliCommand a where
   toCliCommand :: a -> String
 
 instance ToCliCommand Lib1.Command where
   toCliCommand :: Lib1.Command -> String
-  toCliCommand (Lib1.AddFolder path name) = "AddFolder " ++ path ++ " " ++ name
-  toCliCommand _ = "<other command>"
+  toCliCommand (Lib1.Dump Lib1.Examples) = "Dump Examples"
+  toCliCommand Lib1.PrintFile = "PrintFile"
+  toCliCommand (Lib1.EditFile name) = "EditFile " ++ name
 
--- Eq instance (minimal for now)
+-- Manual equality
 instance Eq Lib1.Command where
   (==) :: Lib1.Command -> Lib1.Command -> Bool
-  (Lib1.AddFolder p1 n1) == (Lib1.AddFolder p2 n2) = p1 == p2 && n1 == n2
+  (Lib1.Dump Lib1.Examples) == (Lib1.Dump Lib1.Examples) = True
+  Lib1.PrintFile == Lib1.PrintFile = True
+  (Lib1.EditFile a) == (Lib1.EditFile b) = a == b
   _ == _ = False

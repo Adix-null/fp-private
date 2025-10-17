0b7872fc6a028de08c38c854027c1c1ff8df0ff1
Adomas <adomas@stalas.lt>
2025-10-17 21:14:55 +0000

    boilerplate done for now

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -2,64 +2,124 @@ module Lib1
   ( examples,
     Command (..),
     Dumpable (..),
+    FS (..),
     File (..),
+    Folder (..),
+    FolderRec (..),
+    FileRec (..),
+    FileRec2 (..),
+    Name (..),
+    AlphanumStr (..),
+    AzAZ09 (..),
+    Extension (..),
+    Symbol (..),
+    ASCII (..),
+    Data (..),
+    Path (..),
+    AddFile (..),
+    MoveFile (..),
+    DeleteFile (..),
+    AddFolder (..),
+    MoveFolder (..),
+    DeleteFolder (..),
+    stringToAlphanumStr,
+    stringToPath,
   )
 where
 
+import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
 import GHC.Generics (D)
 
 data Dumpable = Examples
   deriving (Show)
 
-type AlphanumStr = String
-
-type Name = String
-
-type DataStr = String
-
-type Path = String
-
-data File = File
-  { name :: Name,
-    fileData :: DataStr
-  }
+-- <FS> ::= <filerec> | <folderrec>
+data FS
+  = FileRec FileRec
+  | FolderRec FolderRec
   deriving (Show, Eq)
 
-data Folder = Folder
-  { folderName :: AlphanumStr,
-    contents :: [FolderContent]
-  }
+-- <file> ::= <name> "#" <data>
+data File
+  = File Name Data
   deriving (Show, Eq)
 
-data FolderContent
-  = FileRec File
-  | FolderRec Folder
+-- <folder> ::= <alphanumstr> " -> [\n" <FS> "\n]\n"
+data Folder
+  = Folder AlphanumStr FS
   deriving (Show, Eq)
 
+-- <folderrec> ::= <folder> | <folderrec> <folder>
+data FolderRec
+  = SingleFolder Folder
+  | RecFolder Folder FolderRec
+  deriving (Show, Eq)
+
+-- <filerec> ::= "null" | <file> | <filerec2> "\t" <file>
+data FileRec
+  = NullFile
+  | SingleFile File
+  | RecFile FileRec2 File
+  deriving (Show, Eq)
+
+-- <filerec2> ::= <file> | <filerec2> "\t" <file>
+data FileRec2
+  = SingleFile2 File
+  | RecFile2 FileRec2 File
+  deriving (Show, Eq)
+
+-- <name> ::= <alphanumstr> "." <extension>
+data Name
+  = Name AlphanumStr Extension
+  deriving (Show, Eq)
+
+-- <alphanumstr> ::= <azAZ09> | <azAZ09> <alphanumstr>
+data AlphanumStr
+  = Single AzAZ09
+  | Rec AzAZ09 AlphanumStr
+  deriving (Show, Eq)
+
+-- <azAZ09> ::= [a-z] | [A-Z] | [0-9]
 data AzAZ09
   = Lower Char
   | Upper Char
   | Digit Char
   deriving (Show, Eq)
 
+-- <extension> ::= "txt" | "png" | "jpg" | "json" | "dat" | "exe" | "hs" | "cs" | "html" | "cpp" | "mp4" | "mp3"
+data Extension
+  = Txt
+  | Png
+  | Jpg
+  | Json
+  | Dat
+  | Exe
+  | Hs
+  | Cs
+  | Html
+  | Cpp
+  | Mp4
+  | Mp3
+  deriving (Show, Eq)
+
+-- <symbol> ::=  "\t" | "!" | "\"" | "$" | "%" | "&" | "'" | "(" | ")" | "*" | "+" | "," | "-" | "." | ":" | ";" | "<" | "=" | ">" | "?" | "@" | "\\" | "^" | "_" | "`" | "{" | "|" | "}" | "~"
 data Symbol
   = SymTab
-  | SymExcl
+  | SymExclam
   | SymQuote
   | SymDollar
   | SymPercent
-  | SymAmp
-  | SymApos
+  | SymAmpersand
+  | SymApostrophe
   | SymLParen
   | SymRParen
-  | SymStar
+  | SymAsterisk
   | SymPlus
   | SymComma
   | SymMinus
   | SymDot
-  | SymSlash
   | SymColon
-  | SymSemi
+  | SymSemicolon
   | SymLt
   | SymEq
   | SymGt
@@ -75,45 +135,105 @@ data Symbol
   | SymTilde
   deriving (Show, Eq)
 
-data Ascii
-  = Aza AzAZ09
-  | Sym Symbol
+-- <ascii> ::= <azAZ09> | <symbols>
+data ASCII
+  = ASCII AzAZ09 Symbol
   deriving (Show, Eq)
 
-data FileExt
-  = Txt
-  | Png
-  | Jpg
-  | Json
-  | Dat
-  | Exe
-  | Hs
-  | Cs
-  | Html
-  | Cpp
-  | Mp4
-  | Mp3
-  deriving (Show, Eq, Enum, Bounded)
+-- <data> ::= <ascii> | <data> <ascii>
+data Data
+  = SingleASCII ASCII
+  | RecASCII ASCII Data
+  deriving (Show, Eq)
 
--- Available commands
+-- <path> ::= <alphanumstr> | <alphanumstr> "/" <path>
+data Path
+  = SinglePath AlphanumStr
+  | RecPath AlphanumStr Path
+  deriving (Show, Eq)
+
+-- <command> ::= <AddFile> | <MoveFile> | <DeleteFile> | <AddFolder> | <MoveFolder> | <DeleteFolder>
 data Command
   = Dump Dumpable
-  | AddFile Path String
-  | MoveFile Path Path String
-  | DeleteFile Path String
-  | AddFolder Path AlphanumStr
-  | MoveFolder Path Path
-  | DeleteFolder Path
+  | AddFile AddFile
+  | MoveFile MoveFile
+  | DeleteFile DeleteFile
+  | AddFolder AddFolder
+  | MoveFolder MoveFolder
+  | DeleteFolder DeleteFolder
   deriving (Show)
+
+-- <AddFile> ::= "AddFile " <path> " " <file>
+data AddFile
+  = AddFileC Path File
+  deriving (Show, Eq)
+
+-- <MoveFile> ::= "MoveFile " <path> " " <path> " " <name>
+data MoveFile
+  = MoveFileC Path Path Name
+  deriving (Show, Eq)
+
+-- <DeleteFile> ::= "DeleteFile " <path> " " <name>
+data DeleteFile
+  = DeleteFileC Path Name
+  deriving (Show, Eq)
+
+-- <AddFolder> ::= "AddFolder " <path> " " <alphanumstr>
+data AddFolder
+  = AddFolderC Path AlphanumStr
+  deriving (Show, Eq)
+
+-- <MoveFolder> ::= "MoveFolder " <path> " " <path>
+data MoveFolder
+  = MoveFolderC Path Path
+  deriving (Show, Eq)
+
+-- <DeleteFolder> ::= "DeleteFolder " <path>
+data DeleteFolder
+  = DeleteFolderC Path
+  deriving (Show, Eq)
+
+stringToAlphanumStr :: String -> AlphanumStr
+stringToAlphanumStr ch =
+  case ch of
+    [c] -> Single (charToAzAZ09 c)
+    (c : cs) -> Rec (charToAzAZ09 c) (stringToAlphanumStr cs)
+    [] -> error "empty string not allowed"
+  where
+    charToAzAZ09 :: Char -> AzAZ09
+    charToAzAZ09 l =
+      case l of
+        _ | isAsciiLower l -> Lower l
+        _ | isAsciiUpper l -> Upper l
+        _ | isDigit l -> Digit l
+        _ -> error $ "Invalid character for AlphanumStr: " ++ [l]
+
+stringToPath :: String -> Path
+stringToPath s =
+  let splitArray = splitOn '/' s
+   in buildPath splitArray
+  where
+    buildPath :: [String] -> Path
+    buildPath [p] = SinglePath (stringToAlphanumStr p)
+    buildPath (p : ps) = RecPath (stringToAlphanumStr p) (buildPath ps)
+    buildPath [] = error "empty path"
+
+    splitOn :: Char -> String -> [String]
+    splitOn _ [] = [""]
+    splitOn delim (c : cs)
+      | c == delim = "" : rest
+      | otherwise = (c : head rest) : tail rest
+      where
+        rest = splitOn delim cs
 
 -- Example commands
 examples :: [Command]
 examples =
   [ Dump Examples,
-    AddFile "a/b/c" "WS.exe#Px",
-    MoveFile "0/YC" "RV/c9" "3.html",
-    DeleteFile "i" "3m.txt",
-    AddFolder "x/y/z" "GM",
-    MoveFolder "p/a/t/h5" "f1/f2",
-    DeleteFolder "pt/6q"
+    AddFile (AddFileC (stringToPath "a/b/c") (File (Name (stringToAlphanumStr "WS") Exe) (RecASCII (ASCII (Upper 'P') SymExclam) (SingleASCII (ASCII (Upper 'x') SymTab))))),
+    MoveFile (MoveFileC (stringToPath "0/YC") (stringToPath "RV/c9") (Name (stringToAlphanumStr "3") Html)),
+    DeleteFile (DeleteFileC (stringToPath "path/from") (Name (stringToAlphanumStr "3m") Txt)),
+    AddFolder (AddFolderC (stringToPath "x/y/z") (stringToAlphanumStr "Fold9Name")),
+    MoveFolder (MoveFolderC (stringToPath "path/from") (stringToPath "f1/f2")),
+    DeleteFolder (DeleteFolderC $ stringToPath "path/from")
   ]
--- src/Lib2.hs
+++ src/Lib2.hs
@@ -103,29 +103,45 @@ parseFilename =
   pmap (\(name, _dot, ext) -> name ++ "." ++ ext) $
     and3 parseAlphaNumStr (keyword ".") parseExt
 
--- parseEditFile :: Parser ErrorMsg Lib1.Command
--- parseEditFile input =
---   case and3 (keyword "EditFile") ws parseFilename input of
---     Left e -> Left e
---     Right ((_, _, fname), rest) ->
---       if null rest
---         then Right (Lib1.EditFile fname, rest)
---         else Left $ "Bad extension name " ++ rest
+
+parseAddFile :: Parser Lib1.Command
+parseAddFile =
+
+-- parseMoveFile :: Parser Lib1.Command
+-- parseAddFile =
+
+-- parseDeleteFile :: Parser Lib1.Command
+-- parseDeleteFile =
+
+-- parseAddFolder :: Parser Lib1.Command
+-- parseAddFolder =
+
+-- parseMoveFolder :: Parser Lib1.Command
+-- parseMoveFolder =
+
+-- parseDeleteFolder :: Parser Lib1.Command 
+-- parseDeleteFolder = 
 
 -- | Parses user's input.
 -- The function must be implemented and must have tests.
-parseCommand :: Parser ErrorMsg Lib1.Command
--- parseCommand = parseEditFile
+parseCommand :: Parser Lib1.Command
+parseCommand =
+    parseAddFile
+    -- `orElse` parseMoveFile
+    -- `orElse` parseDeleteFile
+    -- `orElse` parseAddFolder
+    -- `orElse` parseMoveFolder
+    -- `orElse` parseDeleteFolder
+parseCommand _ = Left "Not implemented"
 
 process :: Lib1.Command -> [String]
 process (Lib1.Dump Lib1.Examples) = "EXAMPLES:" : map toCliCommand Lib1.examples
--- process (Lib1.EditFile "test.txt") = ["EXAMPLE EDIT: test.txt"]
-process (Lib1.AddFile "path/from" "filedata") = "Example AddFile: " : map toCliCommand "path/from filedata"
-process (Lib1.MoveFile "path/from" "path/to" "filename") = "Example MoveFile: " : map toCliCommand "path/from path/to filename"
-process (Lib1.DeleteFile "path/from" "filename") = "Example DeleteFile: " : map toCliCommand "path/from filename"
-process (Lib1.AddFolder "path/from" "foldername") = "Example AddFolder: " : map toCliCommand "path/from foldername"
-process (Lib1.MoveFolder "path/from" "path/to") = "Example MoveFolder: " : map toCliCommand "path/from path/to"
-process (Lib1.DeleteFolder "path/from") = "Example DeleteFolder: " : map toCliCommand "path/from"
+process (Lib1.AddFile (Lib1.AddFileC path file)) = "Example AddFile: " : [toCliCommand (Lib1.AddFile (Lib1.AddFileC path file))]
+process (Lib1.MoveFile (Lib1.MoveFileC from to fname)) = "Example MoveFile: " : [toCliCommand (Lib1.MoveFile (Lib1.MoveFileC from to fname))]
+process (Lib1.DeleteFile (Lib1.DeleteFileC path fname)) = "Example DeleteFile: " : [toCliCommand (Lib1.DeleteFile (Lib1.DeleteFileC path fname))]
+process (Lib1.AddFolder (Lib1.AddFolderC path folderName)) = "Example AddFolder: " : [toCliCommand (Lib1.AddFolder (Lib1.AddFolderC path folderName))]
+process (Lib1.MoveFolder (Lib1.MoveFolderC from to)) = "Example MoveFolder: " : [toCliCommand (Lib1.MoveFolder (Lib1.MoveFolderC from to))]
+process (Lib1.DeleteFolder (Lib1.DeleteFolderC path)) = "Example DeleteFolder: " : [toCliCommand (Lib1.DeleteFolder (Lib1.DeleteFolderC path))]
 process c = ["Parsed as " ++ show c]
 
 class ToCliCommand a where
@@ -135,26 +151,85 @@ class ToCliCommand a where
 -- Please remove all custom Show instances of Command ADT and
 -- use "deriving Show" only.
 instance ToCliCommand Lib1.Command where
-  toCliCommand :: Lib1.Command -> String
-  -- toCliCommand (Lib1.EditFile fname) = "EditFile " ++ fname
-  toCliCommand (Lib1.AddFile pf d) = "AddFile " ++ pf ++ " " ++ d
-  toCliCommand (Lib1.MoveFile pf pt fln) = "MoveFile " ++ pf ++ " " ++ pt ++ " " ++ fln
-  toCliCommand (Lib1.DeleteFile pf fln) = "DeleteFile " ++ pf ++ " " ++ fln
-  toCliCommand (Lib1.AddFolder pf fdn) = "AddFile " ++ pf ++ " " ++ fdn
-  toCliCommand (Lib1.MoveFolder pf pt) = "MoveFile " ++ pf ++ " " ++ pt ++ " "
-  toCliCommand (Lib1.DeleteFolder pf) = "DeleteFile " ++ pf
+  toCliCommand (Lib1.AddFile (Lib1.AddFileC path file)) = "AddFile " ++ showPath path ++ " " ++ showFile file
+  toCliCommand (Lib1.MoveFile (Lib1.MoveFileC from to fname)) = "MoveFile " ++ showPath from ++ " " ++ showPath to ++ " " ++ showName fname
+  toCliCommand (Lib1.DeleteFile (Lib1.DeleteFileC path fname)) = "DeleteFile " ++ showPath path ++ " " ++ showName fname
+  toCliCommand (Lib1.AddFolder (Lib1.AddFolderC path folderName)) = "AddFolder " ++ showPath path ++ " " ++ showFolderName folderName
+  toCliCommand (Lib1.MoveFolder (Lib1.MoveFolderC from to)) = "MoveFolder " ++ showPath from ++ " " ++ showPath to
+  toCliCommand (Lib1.DeleteFolder (Lib1.DeleteFolderC path)) = "DeleteFolder " ++ showPath path
   toCliCommand (Lib1.Dump d) = "Dump " ++ show d
+
+-- Helper functions to show components
+showFile :: Lib1.File -> String
+showFile (Lib1.File name dat) = showName name ++ "#" ++ showData dat
+  where
+    showData :: Lib1.Data -> String
+    showData (Lib1.SingleASCII ascii) = showASCII ascii
+    showData (Lib1.RecASCII ascii rest) = showASCII ascii ++ showData rest
+
+    showASCII :: Lib1.ASCII -> String
+    showASCII (Lib1.ASCII az sym) = showAzAZ09 az ++ showSymbol sym
+
+    showSymbol :: Lib1.Symbol -> String
+    showSymbol sym = case sym of
+      Lib1.SymTab -> "\t"
+      Lib1.SymExclam -> "!"
+      Lib1.SymQuote -> "\""
+      Lib1.SymDollar -> "$"
+      Lib1.SymPercent -> "%"
+      Lib1.SymAmpersand -> "&"
+      Lib1.SymApostrophe -> "'"
+      Lib1.SymLParen -> "("
+      Lib1.SymRParen -> ")"
+      Lib1.SymAsterisk -> "*"
+      Lib1.SymPlus -> "+"
+      Lib1.SymComma -> ","
+      Lib1.SymMinus -> "-"
+      Lib1.SymDot -> "."
+      Lib1.SymColon -> ":"
+      Lib1.SymSemicolon -> ";"
+      Lib1.SymLt -> "<"
+      Lib1.SymEq -> "="
+      Lib1.SymGt -> ">"
+      Lib1.SymQMark -> "?"
+      Lib1.SymAt -> "@"
+      Lib1.SymBackslash -> "\\"
+      Lib1.SymCaret -> "^"
+      Lib1.SymUnderscore -> "_"
+      Lib1.SymBacktick -> "`"
+      Lib1.SymLCurly -> "{"
+      Lib1.SymPipe -> "|"
+      Lib1.SymRCurly -> "}"
+      Lib1.SymTilde -> "~"
+
+showFolderName :: Lib1.AlphanumStr -> String
+showFolderName = showAlphanumStr
+
+showPath :: Lib1.Path -> String
+showPath (Lib1.SinglePath a) = showAlphanumStr a
+showPath (Lib1.RecPath a rest) = showAlphanumStr a ++ "/" ++ showPath rest
+
+showAlphanumStr :: Lib1.AlphanumStr -> String
+showAlphanumStr (Lib1.Single c) = showAzAZ09 c
+showAlphanumStr (Lib1.Rec c cs) = showAzAZ09 c ++ showAlphanumStr cs
+
+showAzAZ09 :: Lib1.AzAZ09 -> String
+showAzAZ09 (Lib1.Lower c) = [c]
+showAzAZ09 (Lib1.Upper c) = [c]
+showAzAZ09 (Lib1.Digit c) = [c]
+
+showName :: Lib1.Name -> String
+showName (Lib1.Name alphanum ext) = showAlphanumStr alphanum ++ "." ++ show ext
 
 -- | You have to make your Command an instance of Eq class.
 -- Usage of "deriving Eq" is forbidden.
 instance Eq Lib1.Command where
   (==) :: Lib1.Command -> Lib1.Command -> Bool
-  -- Lib1.EditFile f1 == Lib1.EditFile f2 = f1 == f2
-  Lib1.AddFile pf1 d1 == Lib1.AddFile pf2 d2 = pf1 == pf2 && d1 == d2
-  Lib1.MoveFile pf1 pt1 fln1 == Lib1.MoveFile pf2 pt2 fln2 = pf1 == pf2 && pt1 == pt2 && fln1 == fln2
-  Lib1.DeleteFile pf1 fln1 == Lib1.DeleteFile pf2 fln2 = pf1 == pf2 && fln1 == fln2
-  Lib1.AddFolder pf1 fdn1 == Lib1.AddFolder pf2 fdn2 = pf1 == pf2 && fdn1 == fdn2
-  Lib1.MoveFolder pf1 pt1 == Lib1.MoveFolder pf2 pt2 = pf1 == pf2 && pt1 == pt2
-  Lib1.DeleteFolder pf1 == Lib1.DeleteFolder pf2 = pf1 == pf2
+  Lib1.AddFile (Lib1.AddFileC pf1 d1) == Lib1.AddFile (Lib1.AddFileC pf2 d2) = pf1 == pf2 && d1 == d2
+  Lib1.MoveFile (Lib1.MoveFileC pf1 pt1 fln1) == Lib1.MoveFile (Lib1.MoveFileC pf2 pt2 fln2) = pf1 == pf2 && pt1 == pt2 && fln1 == fln2
+  Lib1.DeleteFile (Lib1.DeleteFileC pf1 fln1) == Lib1.DeleteFile (Lib1.DeleteFileC pf2 fln2) = pf1 == pf2 && fln1 == fln2
+  Lib1.AddFolder (Lib1.AddFolderC pf1 fdn1) == Lib1.AddFolder (Lib1.AddFolderC pf2 fdn2) = pf1 == pf2 && fdn1 == fdn2
+  Lib1.MoveFolder (Lib1.MoveFolderC pf1 pt1) == Lib1.MoveFolder (Lib1.MoveFolderC pf2 pt2) = pf1 == pf2 && pt1 == pt2
+  Lib1.DeleteFolder (Lib1.DeleteFolderC pf1) == Lib1.DeleteFolder (Lib1.DeleteFolderC pf2) = pf1 == pf2
   Lib1.Dump d1 == Lib1.Dump d2 = d1 == d2
   _ == _ = False
